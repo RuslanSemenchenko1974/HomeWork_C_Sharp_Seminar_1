8c5db4f5f8600b492af878c73d5906dbf4917827
Ruslan <89604587433@mail.ru>
2022-09-01 15:14:11 +0300

    Сравнение чисел

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число:");
+int firstArg = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondArg = Convert.ToInt32(Console.ReadLine());
+    if (firstArg > secondArg)
+        {
+            Console.WriteLine(firstArg);
+        }
+    if (firstArg < secondArg)
+        {
+            Console.WriteLine(secondArg);
+        }
+    if (firstArg == secondArg)
+    Console.WriteLine("Числа равны");
