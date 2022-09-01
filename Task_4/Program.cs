b49cde5aac15e15190f2dae5242370fb1300a0be
Ruslan <89604587433@mail.ru>
2022-09-01 16:51:16 +0300

    Вывод четоых чисел

---
+++ Task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int countNamber = 1;
+while (countNamber<number)
+{
+    countNamber++;
+    if (countNamber%2==0)
+    Console.Write($" {countNamber}");
+}
